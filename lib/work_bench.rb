c313316599d8585438559aaa4790a398de35d3bd
Konstantin Savelyev <konstantin.savelyev@gmail.com>
2011-11-09 18:13:27 +0400

    Мелкие правки

--- lib/work_bench.rb
+++ lib/work_bench.rb
@@ -1,12 +1,12 @@
-require 'workbench/version'
-require 'workbench/dependencies'
-require 'workbench/js_libs'
-require 'workbench/application'
-require 'workbench/cli'
-require 'workbench/server'
-require 'workbench/renderer'
-require 'workbench/haml_helpers'
-require 'workbench/exporter'
+require 'work_bench/version'
+require 'work_bench/dependencies'
+require 'work_bench/js_libs'
+require 'work_bench/application'
+require 'work_bench/cli'
+require 'work_bench/server'
+require 'work_bench/renderer'
+require 'work_bench/haml_helpers'
+require 'work_bench/exporter'
 
 $root = ENV['PWD']
 
