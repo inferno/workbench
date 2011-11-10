6a981ccca002458d1939f59599af23c29dc5a8e7
Konstantin Savelyev <konstantin.savelyev@gmail.com>
2011-11-10 20:05:29 +0400

    Haml tests

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,5 +1,21 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
-describe 'Project' do
-  pending 'Pending'
+describe 'Haml' do
+
+  before do
+    @root = File.dirname(__FILE__) + '/fixtures'
+  end
+
+  it 'должен рендерить HAML файл' do
+    haml = Workbench::Renderer.new "#{@root}/index.haml"
+    haml.render.should == "<h1 style=\"color: red\" title=\"&amp;\">Hello world!</h1>\n"
+  end
+
+  it 'должны верно устанавливаться опции' do
+    haml = Workbench::Renderer.new "#{@root}/index.haml", { :attr_wrapper => "'",:escape_attrs => false }
+    haml.render.should == "<h1 style='color: red' title='&'>Hello world!</h1>\n"
+  end
+
 end
